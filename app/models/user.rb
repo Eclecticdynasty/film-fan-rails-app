73f5da9481932e889db2f7dbf97d300dd7f3d325
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-21 15:09:34 -0500

    password confirmation and validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password 
+  
   has_many :movies
 
-  validates :email, uniqueness: true, presence: true
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, :password, :username, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 8 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221181342) do
+ActiveRecord::Schema.define(version: 20180221193549) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180221181342) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
