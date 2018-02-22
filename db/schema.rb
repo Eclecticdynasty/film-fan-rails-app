5f074a334d8ea066bc4730133fad54f7088b1b0b
Eclecticdynastye <bklyngal1994@gmail.com>
2018-02-21 20:10:06 -0500

    basic login

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20180221193549) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
